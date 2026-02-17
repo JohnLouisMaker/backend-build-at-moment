d920b7cd8c3030a112106ddd06d06f79f16e9d90
JohnLouisMaker <jluisilvenancio@gmail.com>
2026-02-17 13:57:27 -0300

    back-end start project

---
+++ routes/order_routes.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+order_router = APIRouter(prefix="/pedidos", tags=["orders"])
+
+@order_router.get("/")
+async def pedidos():
+    return {"message": "Get all orders"}
