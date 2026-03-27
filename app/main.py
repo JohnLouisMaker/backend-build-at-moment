42d34111b4f3a9a71dcc18d19d103689fe3fe35a
JohnLouisMaker <jluisilvenancio@gmail.com>
2026-03-27 13:19:49 -0300

    feat: implement CORS middleware to enable frontend-backend communication

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.auth_routes import auth_router
 from app.routes.order_routes import order_router
 
-app = FastAPI()
+app = FastAPI( 
+    title="API Python com FastAPI")
+
+#  CONFIG CORS MIDDLEWARE
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",     
+        "http://127.0.0.1:5173",   
+        "http://localhost:3000",     
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],       
+    allow_headers=["*"],             
+)
 
 # INCLUINDO ROTAS
-app.include_router(auth_router)
-app.include_router(order_router)
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(order_router, prefix="/pedidos", tags=["orders"])
 
 
 # ROTA RAIZ
